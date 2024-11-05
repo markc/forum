171a0591e8b213fa8a6e7a4c11d29c435985dacc
Mark Constable <markc@renta.net>
2024-11-06 07:55:59 +1000

    Make factories and seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,31 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->withPersonalTeam()->create();
+        $users = User::factory(10)->withPersonalTeam()
+            ->has(Post::factory(10))
+            ->create();
 
-        User::factory()->withPersonalTeam()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $posts = Post::factory(200)->recycle($users)->create();
+
+        Comment::factory(100)->recycle($users)->recycle($posts)->create();
+
+        User::factory()
+            ->withPersonalTeam()
+            ->has(Post::factory(45))
+            ->has(Comment::factory(120))->recycle($posts)
+            ->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
     }
 }
